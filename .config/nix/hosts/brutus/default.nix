de89c3bd745a9ada6aab252682852ab5ee8e11d2
cockburncolin <51009168+cockburncolin@users.noreply.github.com>
2025-05-17 19:34:23 -0700

    add config for laptop

--- .config/nix/hosts/brutus/default.nix
+++ .config/nix/hosts/brutus/default.nix
@@ -1,1 +1,20 @@
-{}
+# template for hosts
+{...}: {
+  imports = [
+    ../common
+    ./hardware-config.nix
+  ];
+
+  config = {
+    networking.hostName = "brutus";
+    time.timeZone = "America/Vancouver";
+    age.identityPaths = ["/home/colin/.ssh/id_ed25519"];
+    # Custom modules
+    custom = {
+      emacs.enable = true;
+      sddm.enable = true;
+      wifi.enable = true;
+      wm.enable = true;
+    };
+  };
+}
