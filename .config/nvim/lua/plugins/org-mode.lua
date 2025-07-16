afa9bc27d3410652df1521189491413d368c5caa
cockburncolin <51009168+cockburncolin@users.noreply.github.com>
2025-07-16 15:50:38 -0700

    wip
    Former-commit-id: 21f4f40853608935055f991cf599a7aea1268f0a

--- .config/nvim/lua/plugins/org-mode.lua
+++ .config/nvim/lua/plugins/org-mode.lua
@@ -6,6 +6,10 @@ return {
 		require("orgmode").setup({
 			org_agenda_files = "~/org/**/*",
 			org_default_nodes_file = "~/org/refile.org",
+      org_startup_folded = "inherit",
+      org_todo_keyword_faces = {
+        TODO = ":foreground green :weight bold"
+      },
 		})
 	end,
 }
